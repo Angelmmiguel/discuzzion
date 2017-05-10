b01253d08c2794f99d53ece2092df710c8da4491
Angel M De Miguel <angel@bitnami.com>
2017-05-10 18:05:06 +0200

    Add optional basic auth

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express'),
- app = express();
+  basicAuth = require('express-basic-auth'),
+  app = express();
+
+// Add authentication only if the ENV variable is present
+// The format must be user1=password1,user2=password2...
+if (process.env.BASIC_AUTH_USERS) {
+  let users = {};
+
+  process.env.BASIC_AUTH_USERS.split(',').forEach(userString => {
+    let user = userString.split('=');
+    users[user[0]] = user[1];
+  }, this);
+
+  // Apply the middleware
+  app.use(basicAuth({ users: users }));
+}
 
 // Port
 const port = process.env.PORT || 3001;

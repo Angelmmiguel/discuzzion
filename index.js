ff7a21a10e3f77e306ec7fcfd69c12dfa555bdbb
Angel M De Miguel <angel@bitnami.com>
2017-05-10 15:01:43 +0200

    Receive the port of the application

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express'),
  app = express();
 
+// Port
+const port = process.env.PORT || 3001;
+
 // Static files
 app.use(express.static('build'));
 
@@ -12,9 +15,11 @@ app.get('/status.json', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.get('*', function(req, res) {
+app.get('*', (req, res) => {
   res.sendfile(__dirname + '/build/index.html');
 });
 
 // Create the server
-app.listen(3001);
+app.listen(port, () => {
+  console.log('Our app is running on http://localhost:' + port);
+});
